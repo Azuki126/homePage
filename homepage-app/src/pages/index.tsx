fc4bfa3c10f9b6db009c2447fc155f8c943deeac
Azuki126 <hasukatto.k@icloud.com>
2023-04-15 08:52:40 +0900

    migi

--- homepage-app/src/pages/index.tsx
+++ homepage-app/src/pages/index.tsx
@@ -7,13 +7,13 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <main>
-      <div className="text-center">
-        <p>学習部屋</p>
-        <p>Next.jsとvercelを使用</p>
-        <p>↓クリック</p>
-      <Link href="/profile" className="bg-green-300">
-        自己紹介
-      </Link>
+      <div className="text-center flex flex-col space-y-4">
+        <div className="font-bold text-2xl">2023/4/15</div>
+        <div>気が向いたら更新予定</div>
+        <div>↓クリック</div>
+        <Link href="/profile" className="bg-green-300">
+          自己紹介
+        </Link>
 
       </div>
 
--- homepage-app/src/pages/profile.tsx
+++ homepage-app/src/pages/profile.tsx
@@ -5,13 +5,20 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Prof() {
-  return (
-    <main>
-      <div className="flex flex-col space-y-4 text-center">
-        <div className="bg-green-300">name : マイケル</div>
-        <div className="bg-blue-300">age : 19</div>
-        <div className="bg-red-300">hobby : だらだら</div>
-      </div>
-    </main>
-  )
+    return (
+        <main>
+            <div className="flex flex-col space-y-4 text-center bg-green-300">
+                <div className="">名前 : unknown</div>
+                <div className="">年齢 : 19</div>
+                <div className="">趣味 : アニメ</div>
+                <div>最近のトレンド : 韻波句徒が無限に流れてる</div>
+                <div className="flex justify-center">
+                    <Image src="/m.jpg" alt="namatamago" height={720} width={1280} priority={true}/>
+                </div>
+            </div>
+            <Link href="/" className="bg-green-300">
+                ホームへ戻る
+            </Link>
+        </main>
+    )
 }
