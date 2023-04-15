2f83a25dcddaa3136e28507882ba8257b32b38cd
Azuki126 <hasukatto.k@icloud.com>
2023-04-15 16:01:19 +0900

    update

--- homepage-app/src/pages/index.tsx
+++ homepage-app/src/pages/index.tsx
@@ -1,21 +1,34 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import { Inter } from 'next/font/google'
+import React, {useState} from "react";
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const [text, setText] = useState("");
+  const [addText, setAddText] = useState("");
+
+  const onClickAddText = () => {
+    setAddText(text);
+    setText("");
+  }
   return (
     <main>
       <div className="text-center flex flex-col space-y-4">
-        <div className="font-bold text-2xl">2023/4/15</div>
+        <div className="font-bold text-2xl">2023/4/15更新</div>
         <div>気が向いたら更新予定</div>
         <div>↓クリック</div>
         <Link href="/profile" className="bg-green-300">
           自己紹介
         </Link>
-
+        <div className="space-x-4">
+          <input value={text} onChange={(event) => setText(event.target.value)} className="border-2 h-16" />
+          <button onClick={onClickAddText} className="border-2">送信</button>
+          <p>{addText}</p>
+        </div>
       </div>
+
 
     </main>
   )
--- homepage-app/src/pages/profile.tsx
+++ homepage-app/src/pages/profile.tsx
@@ -7,14 +7,27 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Prof() {
     return (
         <main>
-            <div className="flex flex-col space-y-4 text-center bg-green-300">
+            <div className="flex flex-col space-y-4 text-center bg-green-300 h-2/3">
                 <div className="">名前 : unknown</div>
                 <div className="">年齢 : 19</div>
                 <div className="">趣味 : アニメ</div>
-                <div>最近のトレンド : 韻波句徒が無限に流れてる</div>
-                <div className="flex justify-center">
-                    <Image src="/m.jpg" alt="namatamago" height={720} width={1280} priority={true}/>
+                <div>
+                    最近のトレンド : 韻波句徒が無限に流れてる
+                    <a href="https://www.youtube.com/watch?v=E08Intw81T0&pp=ygUM6Z-75rOi5Y-l5b6S">
+                        →youtube
+                    </a>
                 </div>
+                <div className="flex justify-center">
+                    <Image src="/m.jpg" alt="namatamago" height={720} width={1280} priority={true} />
+                </div>
+            </div>
+            <div className="text-center space-y-4 border-4 w-1/2 absolute left-1/4">
+                <p>ゲーム:じゃんたま、goose goose duck、シャニマス等</p>
+                <p>バイト:塾講師　数学、プログラミング、英語</p>
+                <p>実は受験生だったりする</p>
+                <p>中野あるまさんのyoutubeを見ることが日課</p>
+                <p>目標:180cm、80kg</p>
+
             </div>
             <Link href="/" className="bg-green-300">
                 ホームへ戻る
